c9683c1e9f97fa416a9be83b1966cf1e3fb0ce37
Paridhi Prisha Saikia <paridhiprishasaikia@gmail.com>
2024-12-09 21:11:27 +0300

    Theme switcher logic + calculator logic

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,26 @@
+// Light dark mode switcher logic
+const dayEl = document.querySelector('.day');
+const nightEl = document.querySelector('.night');
+const htmlEl = document.documentElement;
+dayEl.addEventListener('click', () =>{
+    if(nightEl.classList.contains('fillInactive'))return;
+    dayEl.classList.toggle('fillInactive');
+    nightEl.classList.toggle('fillInactive');
+    htmlEl.classList.remove('dark');
+})
+nightEl.addEventListener('click', () =>{
+    if(dayEl.classList.contains('fillInactive'))return;
+    dayEl.classList.toggle('fillInactive');
+    nightEl.classList.toggle('fillInactive');
+    htmlEl.classList.add('dark');
 
+})
+
+// Calculator logic
 const btn1 = document.querySelector('.btn');
 // Declaring the 3 variables
 let operand1, operand2, operator;
-//Declaring the resutl
+//Declaring the refill-[rgba(115,117,122,0.70)]sutl
 let result;
 // Operation statement variable
 let operationName = "";
@@ -16,35 +34,44 @@ btn1.addEventListener('click', (e) =>{
         clickedData == '÷' ||
         clickedData == 'x'
     ){
+        if(operator)return;
         operator = clickedData;
+        document.querySelector('.display').textContent+= " " + clickedData + " ";
         console.log("The operator is: " + operator)
     }
     else if(clickedData == '='){
+        console.log(operand1, operand2, operator)
         if(operand1 && operand2 && operator){
             if(operator == '%')result = Math.round(+operand1) % Math.round(+operand2);
             else if(operator == '-')result = +operand1 - +operand2;
             else if(operator == '+')result = +operand1 + +operand2;
             else if(operator == 'x')result = +operand1 * +operand2;
-            else if(operator == '÷')result = +operand1 / +operand2;
+            else if(operator == '÷'){
+                result = (+operand1 / +operand2);
+            }
+                
         }
+        operand1 = "";operand2 = "";
         document.querySelector('.result').textContent = result;
-        operand1 = "";operand2 = "";result = null;
+        result = null;
     }
     else if(Number.isInteger(+clickedData)){
         if(!operator){
             operand1 = operand1 == undefined? clickedData: operand1 + clickedData;
             operationName = operand1;
+            document.querySelector('.display').textContent += +clickedData;
         }
         else{
             operand2 = operand2 == undefined? clickedData: operand2 + clickedData;
             operationName += operand2;
+            document.querySelector('.display').textContent += +clickedData;
         } 
         console.log(operand1, operand2)
-        document.querySelector('.display').textContext= operationName;
     }
     else if(clickedData == 'Ac'){
         document.querySelector('.result').textContent = "";
         operand1 = "";operand2 = "";
+        document.querySelector('.display').textContent = "";
         operator = null;
     }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Tiny5', 'sans-serif'],
+    },
   },
   plugins: [],
 }
